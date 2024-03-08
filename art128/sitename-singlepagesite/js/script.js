4e1c269af531c6bf49530f2b6fed4cabf2cf25e1
jessieqc1 <156117682+jessieqc1@users.noreply.github.com>
2024-03-07 15:35:20 -1000

    added single page site

---
+++ art128/sitename-singlepagesite/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+    const menubutton = document.querySelector(".menu-button");
+    const menunav = document.querySelector(".toggle-nav");
+    menubutton.addEventListener("click", function () {
+        if (menunav.getAttribute("data-navstate") === "open") {
+            // if true do this
+            menunav.setAttribute("data-navstate", "closed");
+        } else {
+            // else (false) do this
+            menunav.setAttribute("data-navstate", "open");
+        };
+    });
+
+});
+
+// CLOSE THE MENU NAV WHEN A STICKY NAV LINK IS CLOSED
+
+const fixednavlinks= document.querySelectorAll('.fixed nav a'); 
+const menunav = document.querySelector(".toggle-nav");
+fixednavlinks.forEach(function(el) {
+    el.onclick = function() {
+      menunav.setAttribute("data-navstate", "closed");
+    };
+});
